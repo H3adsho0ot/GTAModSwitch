89a3105e04b8777aff3184894f3ffcf2cad9dc3a
H3adsho0ot <niklas.roll123@gmail.com>
2015-09-05 20:13:42 +0200

    Added drag&drop

---
+++ GTAModSwitch/main.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace GTAModSwitch
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 173);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(26, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "enable";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(208, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "disable";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 428);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ GTAModSwitch/main.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GTAModSwitch
+{
+    public partial class main : Form
+    {
+        private string mTempPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\GTAModSwitch\Save";
+
+
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            listBox1.AllowDrop = true;
+
+            if (!Directory.Exists(mTempPath))
+            {
+                Directory.CreateDirectory(mTempPath);
+            }
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] filePaths = (string[])(e.Data.GetData(DataFormats.FileDrop));
+                foreach (string fileLoc in filePaths)
+                {
+                    if (File.Exists(fileLoc))
+                    {
+                        listBox1.Items.Add(fileLoc);
+                    }
+                    else if (Directory.Exists(fileLoc))
+                    {
+                        addDirectory(fileLoc);
+                    }
+
+                }
+            }
+        }
+
+        private void addDirectory(string aPath)
+        {
+            foreach (string file in Directory.GetFiles(aPath))
+            {
+                if (File.Exists(file))
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+
+            foreach (string directory in Directory.GetDirectories(aPath))
+            {
+                if (Directory.Exists(directory))
+                {
+                    addDirectory(directory);
+                }
+            }
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private string getFilename(string aFullPath)
+        {
+            string[] pathSplit = aFullPath.Split('\\');
+            string fileName = pathSplit[pathSplit.Count() - 1];
+            return fileName;
+        }
+
+        private void enableMods()
+        {
+
+        }
+
+        private void disableMods()
+        {
+            foreach (string file in listBox1.Items)
+            {
+                if (File.Exists(file))
+                {
+                    string newFilePath = mTempPath + "\\" + getFilename(file);
+                    File.Move(file, newFilePath);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            disableMods();
+        }
+    }
+}
